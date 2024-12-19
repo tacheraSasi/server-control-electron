c1845bc37c859f4c637311a51ad40f235b9820ef
tacheraSasi <tacherasasi@gmail.com>
2024-12-19 17:45:43 +0300

    first build

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,5 +1,5 @@
 const Switch = ({ type,checked, onChange }) => (
-    <label htmlFor={`switch-${type}`} className={`relative inline-flex items-center cursor-pointer ${checked ? "bg-blue-600" : "bg-neutral-400"} rounded-full w-14 h-8 transition-colors duration-300`}>
+    <label htmlFor={`switch-${type}`} className={`relative inline-flex items-center cursor-pointer ${checked ? "bg-blue-900" : "bg-neutral-400"} rounded-full w-14 h-8 transition-colors duration-300`}>
       <span htmlFor={`switch-${type}`} className={`transform ${checked ? "translate-x-6" : "translate-x-1"} inline-block w-6 h-6 bg-white rounded-full transition-transform duration-300`} />
       <input
         id={`switch-${type}`}
